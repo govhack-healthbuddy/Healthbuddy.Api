9be6b6f91a905c71cf518b8e808caeb8379a598b
Simon Biber <simon.biber@chamonix.com.au>
2015-07-04 15:10:16 +0930

    Allow origin

--- HealthBuddy.Api/HealthBuddy.Api/App_Start/WebApiConfig.cs
+++ HealthBuddy.Api/HealthBuddy.Api/App_Start/WebApiConfig.cs
@@ -28,6 +28,23 @@ namespace HealthBuddy.Api
             );
 
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
+
+            config.MessageHandlers.Add(new ResponseHandler());
+        }
+    }
+
+    class ResponseHandler : System.Net.Http.MessageProcessingHandler
+    {
+
+        protected override System.Net.Http.HttpRequestMessage ProcessRequest(System.Net.Http.HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            return request;
+        }
+
+        protected override System.Net.Http.HttpResponseMessage ProcessResponse(System.Net.Http.HttpResponseMessage response, System.Threading.CancellationToken cancellationToken)
+        {
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+            return response;
         }
     }
 }
